fc2406851f2eab3cf638bffd897b21a52ef7829f
huyuelong <949910047@qq.com>
2024-03-13 16:35:34 +0800

    完成用户注册功能

---
+++ db/index.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql')
+
+// 创建数据路连接对象
+const db = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '123456',
+    database: 'My_DB01'
+})
+
+// 向外共享 db 数据库连接对象
+module.exports = db
